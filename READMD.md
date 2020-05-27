042517e6fa19527aba3f3e4e9449ca7bc0066d5a
Peggy.Hsiao <Peggy.Hsiao@advantech.com>
2020-05-27 11:57:43 +0800

    [create] validate.js example

---
+++ READMD.md
@@ -0,0 +1,10 @@
+# validate.js
+用來驗證表單的JS函式庫。
+
+## 安裝
+### CDN
+也需要嵌入`JQuery`檔案才可以正常使用`validate.js`。
+```
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
+```
---
+++ main.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('#form-content').validate({
+    rules: {
+      'email': { required: true },
+      'password': { required: true, number: true, digits: true, rangelength: [6, 12] },
+      'password-again': { required: true, equalTo: '#password' }
+    },
+    messages: {
+      'email': { required: '此欄位為必填' },
+      'password': { required: '此欄位為必填', number: '請輸入數字', digits: '請輸入數字', rangelength: '請輸入長度為6-12位數的密碼' },
+      'password-again': { required: '此欄位為必填', equalTo: "輸入值必須和密碼相同" }
+    }
+  })
+});
+
